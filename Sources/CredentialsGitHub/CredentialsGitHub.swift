96535ab337468d8f04a8e6b05932632f93d9d30c
Ira Rosen <IRAR@il.ibm.com>
2016-09-08 13:09:54 +0300

    IBM-Swift/Kitura#722 API docs

--- Sources/CredentialsGitHub/CredentialsGitHub.swift
+++ Sources/CredentialsGitHub/CredentialsGitHub.swift
@@ -23,26 +23,45 @@ import SwiftyJSON
 
 import Foundation
 
+// MARK CredentialsGitHub
+
+/// Authentication using GitHub web login with OAuth.
+/// See [GitHub manual](https://developer.github.com/v3/oauth/#web-application-flow)
+/// for more information.
 public class CredentialsGitHub : CredentialsPluginProtocol {
 
     private var clientId : String
 
     private var clientSecret : String
 
+    /// A URL that GitHub redirects back to.
     public var callbackUrl : String
 
-    /// User-Agent must be set in order to access GitHub API (i.e., to get user profile)
-    /// https://developer.github.com/v3/#user-agent-required
+    /// A User-Agent to be passed along on GitHub API calls.
+    /// User-Agent must be set in order to access GitHub API (i.e., to get user profile).
+    /// See [GitHub manual](https://developer.github.com/v3/#user-agent-required)
+    /// for more information.
     public private(set) var userAgent: String
 
+    /// The name of the plugin.
     public var name : String {
         return "GitHub"
     }
 
+    /// An indication whether the plugin is redirecting or not.
     public var redirecting : Bool {
         return true
     }
 
+    /// Caching of user profile information.
+    public var usersCache : NSCache<NSString, BaseCacheElement>?
+    
+    /// Initialize a `CredentialsGitHub`.
+    ///
+    /// - Parameter clientId: the Client ID of the app in the GitHub Developer applications.
+    /// - Parameter clientSecret: the Client Secret of the app in the GitHub Developer applications.
+    /// - Parameter callbackUrl: a URL that GitHub redirects back to.
+    /// - Returns: an instance of `CredentialsGitHub`.
     public init (clientId: String, clientSecret : String, callbackUrl : String, userAgent: String?=nil) {
         self.clientId = clientId
         self.clientSecret = clientSecret
@@ -50,9 +69,18 @@ public class CredentialsGitHub : CredentialsPluginProtocol {
         self.userAgent = userAgent ?? "Kitura-CredentialsGitHub"
     }
 
-    public var usersCache : NSCache<NSString, BaseCacheElement>?
-
-    /// https://developer.github.com/v3/oauth/#web-application-flow
+    /// Authenticate incoming request using GitHub web login with OAuth.
+    ///
+    /// - Parameter request: the `RouterRequest` object used to get information
+    ///                     about the request.
+    /// - Parameter response: the `RouterResponse` object used to respond to the
+    ///                       request.
+    /// - Parameter options: a dictionary of plugin specific options.
+    /// - Parameter onSuccess: a closure to invoke in case of successful authentication.
+    /// - Parameter onFailure: a closure to invoke in case of authentication failure.
+    /// - Parameter onPass: a closure to invoke when the plugin doesn't recognize the
+    ///                     authentication data in the request.
+    /// - Parameter inProgress: a closure to invoke in the process of redirecting authentication.
     public func authenticate (request: RouterRequest, response: RouterResponse,
                               options: [String:Any], onSuccess: @escaping (UserProfile) -> Void,
                               onFailure: @escaping (HTTPStatusCode?, [String:String]?) -> Void,
