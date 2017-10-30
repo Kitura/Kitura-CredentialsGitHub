601d49f87c392ea6b6acb9ed17d4449445483275
ShihabMehboob <shihab.mehboob1@ibm.com>
2017-10-30 11:50:27 +0000

    Updated for Swift4

--- Package.swift
+++ Package.swift
@@ -24,8 +24,8 @@ let package = Package(
     products: [
             // Products define the executables and libraries produced by a package, and make them visible to other packages.
             .library(
-                name: "KituraCredentials",
-                targets: ["KituraCredentials"]
+                name: "CredentialsGitHub",
+                targets: ["CredentialsGitHub"]
             )
         ],
     dependencies: [
@@ -35,8 +35,8 @@ let package = Package(
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages which this package depends on.
             .target(
-                name: "KituraCredentials",
-                dependencies: ["KituraCredentials"]
+                name: "CredentialsGitHub",
+                dependencies: ["Credentials"]
             )
     ]
 )
